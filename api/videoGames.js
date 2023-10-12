01551c485e3bc4510494f397dcb501c19a96cd4b
divya040392 <divya.praveen.5215@gmail.com>
2023-10-12 11:50:22 -0500

    Done -Video game api

--- api/videoGames.js
+++ api/videoGames.js
@@ -12,8 +12,10 @@ const { getAllVideoGames,
 // GET - /api/video-games - get all video games
 router.get('/', async (req, res, next) => {
     try {
+        //By calling getAllVideoGames we got response and send the reponse
         const videoGames = await getAllVideoGames();
         res.send(videoGames);
+        //error handler
     } catch (error) {
         next(error);
     }
@@ -21,28 +23,52 @@ router.get('/', async (req, res, next) => {
 
 // GET - /api/video-games/:id - get a single video game by id
 router.get('/:id', async (req, res, next) => {
+    //By calling getVideoGamebyId with particular id as a parameter we got response and send the reponse
     try {
-        const videoGame = await getVideoGameById(REPLACE_ME);
+        const {id} = req.params;
+        const videoGame = await getVideoGameById(id);
         res.send(videoGame);
+        //error handler
     } catch (error) {
         next(error);
     }
 });
 
 // POST - /api/video-games - create a new video game
-router.patch('/', async (req, res, next) => {
-    // LOGIC GOES HERE 
+router.post('/', async (req, res, next) => {
+    // By calling createVideoGame with required values we got response and send the response
+    try{
+        const videoGame = await createVideoGame(req.body);
+         res.send(videoGame)
+         //error handler
+    }catch(error){
+        next(error);
+    }
 });
 
 
 // PUT - /api/video-games/:id - update a single video game by id
 router.put('/:id', async (req, res, next) => {
-    // LOGIC GOES HERE 
+    // By calling updateVideoGame with required values we got response and send the response
+    try{
+        const videoGame = await updateVideoGame(req.params.id,req.body);
+        res.send(videoGame);
+        //error handler
+    }catch(error){
+        next(error);
+    }
 });
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 router.delete('/:id', async (req, res, next) => {
-    // LOGIC GOES HERE
+    // By calling deleteVideoGame with required id we got response and send the response
+   try{
+    const videoGame = await deleteVideoGame(req.params.id,req.body);
+    res.send(videoGame);
+    //error handler
+   }catch(error){
+    next(error);
+   }
 });
 
 module.exports = router;
--- db/videoGames.js
+++ db/videoGames.js
@@ -6,8 +6,11 @@ const REPLACE_ME = 'HELP REPLACE ME!!!!';
 // GET - /api/video-games - get all video games
 async function getAllVideoGames() {
     try {
-        const { rows: videoGames } = await client.query(REPLACE_ME);
+        // Get all the rows from videogames table using sql query
+        const { rows: videoGames } = await client.query(`SELECT * FROM videogames`);
+        //return all the videogame value
         return videoGames;
+        //error handler
     } catch (error) {
         throw new Error("Make sure you have replaced the REPLACE_ME placeholder.")
     }
@@ -15,12 +18,15 @@ async function getAllVideoGames() {
 
 // GET - /api/video-games/:id - get a single video game by id
 async function getVideoGameById(id) {
+    // Get the particular value from videogame table by passing id
     try {
         const { rows: [videoGame] } = await client.query(`
             SELECT * FROM videoGames
             WHERE id = $1;
         `, [id]);
+        // return the value
         return videoGame;
+        //error handler
     } catch (error) {
         throw error;
     }
@@ -28,17 +34,71 @@ async function getVideoGameById(id) {
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    // LOGIC GOES HERE
+    // To make a post request we need a values of name, description, price, isStock, isPopular,imgUrl 
+    // values that we get from the body object
+    const {name,description,price,inStock,isPopular,imgUrl} = body;
+    try{
+        //By using insert query with body values create a new row into the videogames table 
+        const {rows : [videoGame]} = await client.query(`
+
+            INSERT INTO videogames(name,description,price,"inStock","isPopular","imgUrl")
+            VALUES($1,$2,$3,$4,$5,$6)
+            RETURNING *;
+       `,[name, description,price,inStock,isPopular,imgUrl]);
+       // return the response
+       return videoGame
+    }
+      // error handler
+    catch(error){
+        throw error;
+    }
 }
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
     // LOGIC GOES HERE
+    const setString = Object.keys(fields).map((key,index) =>`"${key}"=$${index+1}`).join(', ');
+    if(setString.length === 0){
+        return;
+    }
+     //To update a particular value we need an id and new data that we get from body object
+     //using update query with new values make a update to that particular id
+    try{
+        const {rows : [videoGame]} = await client.query(`
+           UPDATE videogames
+           SET ${setString}
+           WHERE id=${id}
+           RETURNING *;        
+        `,Object.values(fields));
+      // return response  
+        return videoGame;
+        //error handler
+    }catch (error){
+        throw error;
+    }
+
+
+
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+    // To delete a particular values we need an id that we get from req.params
+    // Using delete query delete the particular value
+    try{
+        const {rows:[videoGame]} = await client.query(`
+
+        DELETE FROM videogames
+        WHERE id =$1
+        RETURNING *;
+        `,[id]);
+        // return response
+        return videoGame;
+    }
+    // error handler
+    catch(error){
+        throw error;
+    }
 }
 
 module.exports = {
